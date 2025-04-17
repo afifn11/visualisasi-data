d74c31fa19b5d265d1b81cb5c7c12ae88344fea3
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2025-04-17 14:42:16 +0700

    add initial Streamlit application with basic text display

---
+++ praktikum/pertemuan-6/textElemen.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.write("# Hello World!")
+st.title("Hello World!")
+st.header("Hello World!")
+st.subheader("Hello World!")
+st.caption("Hello World!")
+
+# displaying plain text
+st.text("Hello World!")
+st.markdown("Hello World!")
+st.latex(r'''a^2 + b^2 = c^2''')
+st.code(r'''a^2 + b^2 = c^2''', language='latex')
+
