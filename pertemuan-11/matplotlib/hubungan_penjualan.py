7a76ff216762da3d64399e4f24784bbddc26b6d3
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2025-05-22 14:48:13 +0700

    add scatter plot visualization for ice cream sales and temperature

---
+++ pertemuan-11/matplotlib/hubungan_penjualan.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+# Data dummy
+suhu = [20, 22, 24, 26, 28, 30, 32, 34, 36, 38]
+penjualan = [50, 60, 70, 80, 90, 100, 110, 130, 180]
+
+# Membuat scatter plot
+plt.scatter(suhu, penjualan, color='blue')
+plt.title('Hubungan Penjualan Es Krim dan Suhu')
+plt.xlabel('Suhu (°C)')
+plt.ylabel('Penjualan Es Krim')
+# Menampilkan plot
+plt.show()
